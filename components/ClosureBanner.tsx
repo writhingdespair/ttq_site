e2d87bb7b0de94dc7260d2d500d700909021818e
writhingdespair <writhingdespair@gmail.com>
2026-04-30 04:32:56 +0000

    Make closure banner fixed to fully cover header

--- components/ClosureBanner.tsx
+++ components/ClosureBanner.tsx
@@ -16,7 +16,7 @@ export default function ClosureBanner() {
   if (!closed) return null
 
   return (
-    <div className="bg-amber-500 text-black text-center py-3 px-4 text-sm font-medium z-50 sticky top-0">
+    <div className="bg-amber-500 text-black text-center py-3 px-4 text-sm font-medium fixed top-0 left-0 right-0 z-toast">
       We are currently closed. We are open daily 10 AM - 8 PM.
     </div>
   )
